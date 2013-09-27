8c5346b35fbb1173e3e1843cb2872e325d6f2e9b
Mark Tseng <chunjen.tseng@gmail.com>
2013-09-28 01:15:25 +0800

    [M] modify vpn nat.sh script

--- conf/vpn/nat.sh
+++ conf/vpn/nat.sh
@@ -1,2 +1,3 @@
-/sbin/iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth0 -j MASQUERADE
-/sbin/iptables -I FORWARD -p tcp --syn -i ppp+ -j TCPMSS --set-mss 1356
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth0 -j MASQUERADE
+iptables -I FORWARD -p tcp --syn -i ppp+ -j TCPMSS --set-mss 1356
