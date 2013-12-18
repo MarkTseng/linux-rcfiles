3433ab3bfbd939fbf88601c9b971b36e28fa17fe
Mark Tseng <chunjen.tseng@gmail.com>
2013-12-18 14:14:47 +0800

    [M] update script

--- nike-tools/nike_img_dvdplayer-make.sh
+++ nike-tools/nike_img_dvdplayer-make.sh
@@ -25,6 +25,11 @@ cd develop/image_file_avhdd/
 cp ../image_file/components/SQA_DailyBuild/AV_FW/System.map.* components/packages/package2/
 cp ../image_file/components/SQA_DailyBuild/AV_FW/bluecore.audio.zip components/packages/package2/
 cp ../image_file/components/SQA_DailyBuild/AV_FW/bluecore.video.zip components/packages/package2/
+
+# new audio fw
+#cp ../image_file/components/AV_FW/System.map.audio_china_dtmb components/packages/package2/System.map.audio
+#cp ../image_file/components/AV_FW/bluecore.audio.zip_china_dtmb components/packages/package2/bluecore.audio.zip
+
 rm -rf components/packages/package2/AP/bin
 cp -a ../../system/project/Realtek/bin/ components/packages/package2/AP/
 make image install_ap=1 install_bootloader=1 hash_imgfile=1 install_factory=1 factory_file=factory_GS.tar PACKAGES=package2
