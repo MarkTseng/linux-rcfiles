d1a8b737e33d5c92fad169157a60a90ebe151a86
Mark Tseng <chunjen.tseng@gmail.com>
2014-04-08 22:59:01 +0800

    [M] modify java version to 7

--- android_bin/install_android_required_packages.sh
+++ android_bin/install_android_required_packages.sh
@@ -5,7 +5,7 @@ sudo apt-get install git gnupg flex bison gperf build-essential \
   libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
   libgl1-mesa-dev g++-multilib mingw32 tofrodos \
   python-markdown libxml2-utils xsltproc zlib1g-dev:i386
-
+sudo apt-get install openjdk-7-jdk
 sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
 # install java
