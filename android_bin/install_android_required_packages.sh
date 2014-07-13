8ab6775531887d793b1b41ff07f37ea65a790bed
Mark Tseng <chunjen.tseng@gmail.com>
2014-07-13 22:38:19 +0800

    [M] remove libmesa

--- android_bin/install_android_required_packages.sh
+++ android_bin/install_android_required_packages.sh
@@ -2,11 +2,11 @@
 # required packages
 sudo apt-get install git gnupg flex bison gperf build-essential \
   zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
-  libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
+  libx11-dev:i386 libreadline6-dev:i386 \
   libgl1-mesa-dev g++-multilib mingw32 tofrodos \
   python-markdown libxml2-utils xsltproc zlib1g-dev:i386
-sudo apt-get install openjdk-7-jdk
-sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
+#sudo apt-get install openjdk-7-jdk
+#sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
 # install java
 sudo add-apt-repository ppa:webupd8team/java
