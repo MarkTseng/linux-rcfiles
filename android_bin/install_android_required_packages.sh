21c69dce38fdc00036bae40046a68c3ab5f5c7e8
Mark Tseng <chunjen.tseng@gmail.com>
2014-07-15 10:43:49 +0800

    [M] remove libncurses5-dev:i386

--- android_bin/install_android_required_packages.sh
+++ android_bin/install_android_required_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # required packages
 sudo apt-get install git gnupg flex bison gperf build-essential \
-  zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
+  zip curl libc6-dev x11proto-core-dev \
   libx11-dev:i386 libreadline6-dev:i386 \
   libgl1-mesa-dev g++-multilib mingw32 tofrodos \
   python-markdown libxml2-utils xsltproc zlib1g-dev:i386
