ce38683caae9e4ce711f48519f6d677665755613
mark.tseng <mark.tseng@realtek.com>
2015-05-05 16:36:09 +0800

    [D] remove pypy

--- pypy/pypy_install.sh
+++
@@ -1,6 +0,0 @@
-wge	https://bitbucket.org/pypy/pypy/downloads/pypy-2.3.1-src.tar.bz2
-sudo apt-get install \
-gcc make python-dev libffi-dev libsqlite3-dev pkg-config \
-libz-dev libbz2-dev libncurses-dev libexpat1-dev \
-libssl-dev libgc-dev python-sphinx python-greenlet
-
