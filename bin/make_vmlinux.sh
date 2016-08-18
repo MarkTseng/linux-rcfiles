cd978747babf7b99a79cf4b1a7910969d950e9b8
mark <mark.tseng@mstarsemi.com>
2016-08-18 11:44:00 +0800

    [A] add ubuntu 16.04 branch and bug fixed

--- bin/make_vmlinux.sh
+++ bin/make_vmlinux.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-make -j 8
-make modules_install INSTALL_MOD_PATH=rootfs
-make 
-cp vmlinux.bin  ~/tftp
+#make -j 8
+#make modules_install INSTALL_MOD_PATH=rootfs
+#make 
+#cp vmlinux.bin  ~/tftp
 #cp vmlinux  ~/tftp
-
+make -j4
+cp -a arch/arm/boot/Image ../../release/SDK/arm/kernel/I1/LX318/arch/arm/boot/Image
+cp -a modules/*.ko ../../release/SDK/arm/kernel/I1/LX318/modules
+pushd ../../project/I1
+make clean
+make
+popd
+sync
