d592504d955a1a17aaed94a965f2e2e46bea7fdb
mark <chunjen.tseng@gmail.com>
2018-10-01 01:30:08 +0800

    [M] rename brew_install.sh -> macosx_install.sh

--- macosx_install.sh
+++ macosx_install.sh
@@ -1,2 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install findutils coreutils htop tig gawk git vim 
+ln -s $HOME/linux-rcfiles/rcfile/bashrc $HOME/.bash_profile
+ln -s $HOME/linux-rcfiles/rcfile/vimrc $HOME/.vimrc
+
