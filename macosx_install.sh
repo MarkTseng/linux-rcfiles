339748908a6eb72a4e456dd1eff544daf555b7d3
mark <chunjen.tseng@gmail.com>
2018-10-01 01:42:21 +0800

    [A] add mongodb in macosx_install.sh

--- macosx_install.sh
+++ macosx_install.sh
@@ -1,5 +1,9 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install findutils coreutils htop tig gawk git vim 
+brew install findutils coreutils htop tig gawk git vim mongodb
 ln -s $HOME/linux-rcfiles/rcfile/bashrc $HOME/.bash_profile
 ln -s $HOME/linux-rcfiles/rcfile/vimrc $HOME/.vimrc
+
+# mongodb
+mkdir -p /data/db
+sudo chown -R `id -un` /data/db
 
