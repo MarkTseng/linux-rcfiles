c3e89213073cd69dbc679b3eb647cc28f13b8d55
Mark Tseng <chunjen.tseng@gmail.com>
2018-10-08 21:03:35 +0800

    [M] fixed macosx_install.sh bug     update rcfile in when use brew     update vimplugin.tgz with go pluging

--- macosx_install.sh
+++ macosx_install.sh
@@ -1,9 +1,9 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install findutils coreutils htop tig gawk git vim mongodb
+brew install findutils coreutils htop tig gawk git vim mongodb cowsay fortune
 ln -s $HOME/linux-rcfiles/rcfile/bashrc $HOME/.bash_profile
 ln -s $HOME/linux-rcfiles/rcfile/vimrc $HOME/.vimrc
 
 # mongodb
-mkdir -p /data/db
+sudo mkdir -p /data/db
 sudo chown -R `id -un` /data/db
 
