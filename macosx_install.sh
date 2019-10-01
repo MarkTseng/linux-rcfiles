a79d0b8f3cc4f231c8e97515a9ae41a9b877b580
Mark Tseng <chunjen.tseng@gmail.com>
2019-10-02 01:37:56 +0800

    [M] fixed mac os x mojave brew environment

--- macosx_install.sh
+++ macosx_install.sh
@@ -1,9 +1,13 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install findutils coreutils htop tig gawk git vim mongodb cowsay fortune
+brew install findutils coreutils htop tig gawk git vim cowsay fortune
 ln -s $HOME/linux-rcfiles/rcfile/bashrc $HOME/.bash_profile
 ln -s $HOME/linux-rcfiles/rcfile/vimrc $HOME/.vimrc
+tar xfz vimplug.tar -C $HOME/
 
 # mongodb
+brew tap mongodb/brew
+brew install mongodb-community
+brew install mongodb-community-shell
 sudo mkdir -p /data/db
 sudo chown -R `id -un` /data/db
-
+brew services start mongodb-community
