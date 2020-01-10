8043899884cab6fdb18ed621850f2916b8c14d65
Mark Tseng <chun-jen.tseng@mediatek.com>
2020-01-10 21:56:01 +0800

    [M] update rcfile and shell script

--- bin/wireless_connect.sh
+++ bin/wireless_connect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo killall wpa_supplicant
-ip link show wlp2s0
+ip link show wlp3s0
 wpa_passphrase 'Tab s' 0423326789 > wpa_connect.conf
-sudo wpa_supplicant -B -D nl80211 -i wlp2s0 -c wpa_connect.conf
-iw wlp2s0 link
-sudo dhclient wlp2s0
+sudo wpa_supplicant -B -D nl80211 -i wlp3s0 -c wpa_connect.conf
+iw wlp3s0 link
+sudo dhclient wlp3s0
 
